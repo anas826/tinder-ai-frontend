c301e4caa69eb2ab22b4a335d83b0c89ef35311b
anas826 <saifianas826@gmail.com>
2024-08-09 18:30:54 +0530

    created matches api call

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,18 @@ const fetchRandomProfile = async () => {
   return response.json();
 }
 
-
+const saveSwipe = async(profileId) => {
+  const response = await fetch('http://localhost:8080/matches',{
+    method: 'POST',
+    headers: {  
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({profileId})
+  });
+  if(!response.ok){
+    throw new Error("Failed to swipe");
+  }
+}
 
 
 const ProfileSelector = ({profile, onSwipe}) => (
@@ -29,11 +40,11 @@ const ProfileSelector = ({profile, onSwipe}) => (
       </div>
     <div className="p-4 flex justify-center space-x-4">
       <button className="bg-red-500 rounded-full p-4 text-white hover:bg-red-700"
-      onClick={()=> onSwipe("left")}>
+      onClick={()=> onSwipe(profile.id, "left")}>
         <X size = {24} />
       </button>
       <button className="bg-green-500 rounded-full p-4 text-white hover:bg-green-700"
-      onClick={()=> onSwipe("right")}>
+      onClick={()=> onSwipe(profile.id, "right")}>
         <Heart size = {24} />
       </button>
     </div>
@@ -137,9 +148,9 @@ function App() {
   const [currentScreen, setCurrentScreen] = useState('profile');
   const [currentProfile, setCurrentProfile] = useState(null);
 
-  const onSwipe =  (direction) =>{
+  const onSwipe =  (profileId, direction) =>{
     if(direction=='right'){
-      console.log('liked');
+      saveSwipe(profileId)
     }
     loadRandomProfile();
   }
